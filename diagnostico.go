3c1e800be82828818fbc0801f159133b76d921f0
giovanna melo <olemgiovanna@gmail.com>
2026-02-18 23:58:14 -0300

    atividade pilares C.E.M

---
+++ diagnostico.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+/* RESPOSTAS DO DIAGNÓSTICO:
+
+1. [ESCALABILIDADE]: o sistema continua funcionando, mas com algumas limitações que evidenciam a falta de preparo para lidar com o volume crescente de dados, tráfego, etc. Sendo assim, nessa falha, se destaca a falta de escalabilidade para lidar com o crescimento do tráfego de solicitações para finalizar a compra.
+
+2. [CONFIABILIDADE]: apesar de estar ligado ao pico de acessos, acredito que esse é um erro gravíssimo que afeta o funcionamento normal do sistema, afetando assim o pilar de confiabilidade. Esse tipo de falha pode e deveria ter sido testado com antecedência.
+
+3. [MANUTENIBILIDADE]: esse pilar refere-se a trabalhar com o código de maneira produtiva e entre as mais diversas equipes. Quando um código é apresentado de maneira complexa e mal documentado, como é dito na descrição do erro 3, significa que sua falha é de Manutenibilidade.
+
+*/
+
+func main() {
+	fmt.Println("Ambiente configurado e desafio analisado! por: Giovanna Cavalcanti Melo \n ")
+
+	fmt.Println("Partiu Carnaval, mas com mindset de Alta Performance! xD")
+	fmt.Scanln()
+}
